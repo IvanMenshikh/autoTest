f530a857953ccfa936ff2fdecebe0df53bbbbb30
Menshikh Ivan A. <ivashkashox@gmail.com>
2024-04-17 15:12:48 +0300

    1 commit

---
+++ src/test/java/Data.java
@@ -0,0 +1,7 @@
+public class Data {
+
+    public String login = "Smoke_user118";
+    public String password = "Test123456";
+    public String urlKSED = "http://172.30.48.40:8080/share/page";
+
+}
