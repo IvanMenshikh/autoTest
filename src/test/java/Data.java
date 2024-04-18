52a572553d1915d53f2bd7cc1ef1c81e46d4bbc2
Menshikh Ivan A. <ivashkashox@gmail.com>
2024-04-18 21:12:05 +0300

    New project on selenide

--- src/test/java/Data.java
+++
@@ -1,8 +0,0 @@
-//Данные для заполнения полей
-public class Data {
-
-    public String login = "Smoke_user118";
-    public String password = "Test123456";
-    private final static String urlKSED = "http://172.30.48.40:8080/share/page";
-
-}
--- src/test/java/ErrandTest.java
+++ src/test/java/ErrandTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class ErrandTest extends BaseTest {
     private final static String ksedURL = "http://172.30.48.40:8080/share/page";
     private final static String loginUser = "Smoke_user118";
-    private final static String passwordUser = "Test123456";
+    private final static String passwordUser = "";
 
     @Test
     public void errand(){
