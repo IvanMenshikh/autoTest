419f986d6345db5442cab14e2dbb5af233b6d5f6
Menshikh Ivan A. <ivashkashox@gmail.com>
2024-04-26 01:15:17 +0300

    Fixing for the method Selenide.lazy

--- src/test/java/ArmPage.java
+++ src/test/java/ArmPage.java
@@ -5,16 +5,38 @@ import static com.codeborne.selenide.Selenide.sleep;
 
 public class ArmPage {
 
-    private final SelenideElement buttonCreate = $x("//button[@id = 'template_x002e_accordion-toolbar_x002e_arm_x0023_default-newDocumentButton-button']");
-    private final SelenideElement buttonDocTypeErrand = $x("//a[@class = 'yuimenuitemlabel yuimenuitemlabel-hassubmenu']");
-    private final SelenideElement buttonDocTypeKS = $x("//a[@class = 'yuimenuitemlabel']");
+    private SelenideElement buttonCreate;
+    private SelenideElement buttonDocTypeKS;
+    private SelenideElement buttonDocTypeErrand;
+
+    private SelenideElement getButtonCreate(){
+        if(buttonCreate == null){
+            buttonCreate = $x("//button[@id = 'template_x002e_accordion-toolbar_x002e_arm_x0023_default-newDocumentButton-button']");
+        }
+        return buttonCreate;
+    }
+    private SelenideElement getButtonDocTypeKS(){
+        if(buttonDocTypeKS == null){
+            buttonDocTypeKS = $x("//a[@class = 'yuimenuitemlabel']");
+        }
+        return buttonDocTypeKS;
+    }
+    private SelenideElement getButtonDocTypeErrand(){
+        if(buttonDocTypeErrand == null){
+            buttonDocTypeErrand = $x("//a[@class = 'yuimenuitemlabel yuimenuitemlabel-hassubmenu']");
+        }
+        return buttonDocTypeErrand;
+    }
+//    private final SelenideElement buttonCreate = $x("//button[@id = 'template_x002e_accordion-toolbar_x002e_arm_x0023_default-newDocumentButton-button']");
+//    private final SelenideElement buttonDocTypeErrand = $x("//a[@class = 'yuimenuitemlabel yuimenuitemlabel-hassubmenu']");
+//    private final SelenideElement buttonDocTypeKS = $x("//a[@class = 'yuimenuitemlabel']");
 
     /**
      * Открываем форму создания поручения
      */
     public void createErrand(){
-        buttonCreate.click();
-        buttonDocTypeErrand.click();
+        getButtonCreate().click();
+        getButtonDocTypeErrand().click();
         sleep(3000);//Останавливаем код для просмотра конечного результата
     }
 
--- src/test/java/AuthorizationPage.java
+++ src/test/java/AuthorizationPage.java
@@ -7,10 +7,43 @@ import static com.codeborne.selenide.Selenide.$x;
  * Страница авторизации КСЭД.
  */
 public class AuthorizationPage {
-    private final SelenideElement login = $x("//div[@class = 'form-field']//input[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-username']");
-    private final SelenideElement password = $x("//div[@class = 'form-field']//input[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-password']");
-    private final SelenideElement buttonAuthorization = $x("//button[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-submit-button']");
-    private final SelenideElement getTitle = $x("//span[@class = 'alfresco-header-Title__text']");
+    private SelenideElement login;
+    private SelenideElement password;
+    private SelenideElement buttonAuthorization;
+    private SelenideElement checkHomeTitle;
+
+    /**
+     * Реализована ленивая инициализация элементов страницы в классе
+     */
+    private SelenideElement getLogin() {
+        if (login == null) {
+            login = $x("//div[@class = 'form-field']//input[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-username']");
+        }
+        return login;
+    }
+    private SelenideElement getPassword(){
+        if(password == null){
+            password = $x("//div[@class = 'form-field']//input[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-password']");
+        }
+        return password;
+    }
+    private SelenideElement getButtonAuthorization(){
+        if(buttonAuthorization == null){
+            buttonAuthorization = $x("//button[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-submit-button']");
+        }
+        return buttonAuthorization;
+    }
+    private SelenideElement getCheckHomeTitle(){
+        if(checkHomeTitle == null){
+            checkHomeTitle = $x("//span[@class = 'alfresco-header-Title__text']");
+        }
+        return checkHomeTitle;
+    }
+
+//    //private final SelenideElement login = $x("//div[@class = 'form-field']//input[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-username']");
+//    private final SelenideElement password = $x("//div[@class = 'form-field']//input[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-password']");
+//    private final SelenideElement buttonAuthorization = $x("//button[@id = 'page_x002e_components_x002e_slingshot-login_x0023_default-submit-button']");
+//    private final SelenideElement getTitle = $x("//span[@class = 'alfresco-header-Title__text']");
     /**
      * Открываем URL через конструктор.
      * @param url
@@ -23,16 +56,16 @@ public class AuthorizationPage {
      * Авторизация с заполнением полей и нажатием на кнопку "Войти".
      */
     public void authorization() {
-        login.setValue("Smoke_user118");
-        password.setValue("");
-        buttonAuthorization.click();
+        getLogin().setValue("Smoke_user118");
+        getPassword().setValue("Test123456");
+        getButtonAuthorization().click();
     }
 
     /**
      * Проверяем, что авторизация прошла успешно.
      */
     public void checkAuthorization(){
-        String armTitle = getTitle.getText();
+        String armTitle = getCheckHomeTitle().getText();
         Assert.assertEquals(armTitle, "АРМ СЭД");
         System.out.println(armTitle);
     }
