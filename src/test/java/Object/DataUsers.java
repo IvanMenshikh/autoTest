5a604cb348b556d406508a3a1b21fe22dabe7f4a
Меньших Иван Андреевич <MenshikhIA@sibintek.ru>
2024-07-11 10:19:58 +0300

    add user

---
+++ src/test/java/Object/DataUsers.java
@@ -0,0 +1,11 @@
+package Object;
+
+public class DataUsers {
+
+    // Временно храню данные тестовых в этом классе. В будущем додавлю использование библиотек для хеширования паролей (BCrypt)
+    public static final String loginIniciator = "Smoke_user118";
+    public static final String passIniciator = "Test123456";
+    public static final String loginNegativeUser = "CheckNegAuth";
+    public static final String passNegativeUser = "Test12345";
+}
+
